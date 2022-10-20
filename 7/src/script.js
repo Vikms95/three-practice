28b718b803557356aa272fae175c3afcc94b4509
Vikms95 <vms1995@hotmail.com>
2022-10-20 22:00:34 +0200

    reach minute 30 from lesson 7

---
+++ 7/src/script.js
@@ -0,0 +1,77 @@
+import './style.css'
+import * as THREE from 'three'
+
+//* Move camera with cursor
+const cursor = {
+  x: 0,
+  y: 0
+}
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600
+}
+
+window.addEventListener('mousemove', e => {
+  // We set the value to go from -0.5 to 0.5
+  cursor.x = e.clientX / sizes.width - 0.5
+  cursor.y = e.clientY / sizes.height - 0.5
+})
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
+// * Perspective camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, .1, 100)
+
+//* Ortographic camera
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, .1, 100)
+
+//
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 2
+camera.lookAt(mesh.position)
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    // mesh.rotation.y = elapsedTime;
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
