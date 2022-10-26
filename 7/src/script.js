c0346b95bd4f259926c57e77a6ef2e2e4f78f34a
Vikms95 <vms1995@hotmail.com>
2022-10-26 19:16:43 +0200

    finish lesson 7

--- 7/src/script.js
+++ 7/src/script.js
@@ -1,22 +1,15 @@
 import './style.css'
 import * as THREE from 'three'
-
-//* Move camera with cursor
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+// ** Cursor
+// Get coordinates of the mouse
 const cursor = {
   x: 0,
   y: 0
 }
-
-// Sizes
-const sizes = {
-  width: 800,
-  height: 600
-}
-
-window.addEventListener('mousemove', e => {
-  // We set the value to go from -0.5 to 0.5
-  cursor.x = e.clientX / sizes.width - 0.5
-  cursor.y = e.clientY / sizes.height - 0.5
+window.addEventListener('mousemove', (e) => {
+  cursor.x = e.clientX / sizes.width - .5
+  cursor.y = -(e.clientY / sizes.height - .5)
 })
 
 /**
@@ -25,7 +18,11 @@ window.addEventListener('mousemove', e => {
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
 
 // Scene
 const scene = new THREE.Scene()
@@ -37,19 +34,31 @@ const mesh = new THREE.Mesh(
 )
 scene.add(mesh)
 
-// * Perspective camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, .1, 100)
+// Camera
+const camera = new THREE.PerspectiveCamera(50, sizes.width / sizes.height, 0.1, 100)
 
-//* Ortographic camera
 // const aspectRatio = sizes.width / sizes.height
-// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, .1, 100)
+// const camera = new THREE.OrthographicCamera(
+//   -1  * aspectRatio,
+//    1 * aspectRatio,
+//   1,
+//   -1,
+//   .1,
+//   100)
 
-//
 // camera.position.x = 2
 // camera.position.y = 2
-camera.position.z = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+// ** Implement a control
+const controls = new OrbitControls(camera, canvas)
+controls.target.y = 1
+
+// Add smoothness to the controls
+controls.enableDamping = true
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -66,6 +75,16 @@ const tick = () =>
 
     // Update objects
     // mesh.rotation.y = elapsedTime;
+
+    // ** Update camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(mesh.position)
+    
+    // When you change the target after instantiating,
+    // you need to call the update method on every render
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
