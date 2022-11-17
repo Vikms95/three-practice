e09515f2433b64618cfd9ed9787147c71e8d7131
Vikms <vms1995@hotmail.com>
2022-11-17 18:17:52 +0100

    finish lesson 22

--- 22 Physics/src/script.js
+++ 22 Physics/src/script.js
@@ -2,30 +2,68 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
-import CANNON from 'cannon'
+import * as CANNON from 'cannon-es'
+
+// Sound
+const hitSound = new Audio( '/sounds/hit.mp3' )
+const playSound = ( collision ) => {
+
+  // Check how hard did the box hit so we can either play the sound or not
+  const impactStrenght = collision.contact.getImpactVelocityAlongNormal()
+
+  if ( impactStrenght > 1.5 ) {
+    hitSound.volume = Math.random() / impactStrenght
+    hitSound.currentTime = 0
+    hitSound.play()
+  }
+}
+
 /**
  * Debug
  */
 const gui = new dat.GUI()
 const debugObject = {}
 
-debugObject.createSphere = () =>
-{
-    createSphere( Math.random() * 0.5,
-        {
-            x: ( Math.random() - 0.5 ) * 3,
-            y: 5 - ( Math.random() - 0.5 ) * 3,
-            z: (Math.random() -  0.5) * 3
-        } )   
+debugObject.createSphere = () => {
+  createSphere( Math.random() * 0.5,
+    {
+      x: ( Math.random() - 0.5 ) * 3,
+      y: 3,
+      z: ( Math.random() - 0.5 ) * 3
+    } )
+}
+
+debugObject.createBox = () => {
+  createBox(
+    Math.random(),
+    Math.random(),
+    Math.random(),
+    {
+      x: ( Math.random() - 0.5 ) * 3,
+      y: 3,
+      z: ( Math.random() - 0.5 ) * 3
+    } )
+}
+
+debugObject.reset = () => {
+  // Delete all objects from the scene and world
+  for ( const object of objectsToUpdate ) {
+    object.body.removeEventListener( 'collide', playSound )
+    world.removeBody( object.body )
+    scene.remove( object.mesh )
+    objectsToUpdate.splice( 0, objectsToUpdate.length )
+  }
 }
 
 gui.add( debugObject, 'createSphere' )
+gui.add( debugObject, 'createBox' )
+gui.add( debugObject, 'reset' )
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector( 'canvas.webgl' )
 
 // Scene
 const scene = new THREE.Scene()
@@ -38,19 +76,21 @@ const cubeTextureLoader = new THREE.CubeTextureLoader()
 
 
 
-const environmentMapTexture = cubeTextureLoader.load([
-    '/textures/environmentMaps/0/px.png',
-    '/textures/environmentMaps/0/nx.png',
-    '/textures/environmentMaps/0/py.png',
-    '/textures/environmentMaps/0/ny.png',
-    '/textures/environmentMaps/0/pz.png',
-    '/textures/environmentMaps/0/nz.png'
-])
+const environmentMapTexture = cubeTextureLoader.load( [
+  '/textures/environmentMaps/0/px.png',
+  '/textures/environmentMaps/0/nx.png',
+  '/textures/environmentMaps/0/py.png',
+  '/textures/environmentMaps/0/ny.png',
+  '/textures/environmentMaps/0/pz.png',
+  '/textures/environmentMaps/0/nz.png'
+] )
 
 //** Physics **/
 
 // Just like a scene in Three
 const world = new CANNON.World()
+world.broadphase = new CANNON.SAPBroadphase( world )
+world.allowSleep = true
 world.gravity.set( 0, -2.82, 0 )
 
 // To change the friction and boucning, we first need to create new instances of Material 
@@ -61,18 +101,18 @@ const defaultMaterial = new CANNON.Material( 'default' )
 // default material. You can use different ones. This is good when you have a simple 3D space
 // with no material specifications
 const defaultContactMaterial = new CANNON.ContactMaterial(
-    defaultMaterial,
-    defaultMaterial,
-    {
-        // The higher the friction, more momentum they will loose when rubbing against each other
-        friction: 0.1,
+  defaultMaterial,
+  defaultMaterial,
+  {
+    // The higher the friction, more momentum they will loose when rubbing against each other
+    friction: 0.1,
 
-        // The higher the restituion, the Bodies will keep its momentum if they collide face to face.
-        // Otherwise they will loose it.
-        // == 1; it wont loose any momentum
-        // > 1; it will gain momentum on each collision
-        restitution: 0.5
-    }
+    // The higher the restituion, the Bodies will keep its momentum if they collide face to face.
+    // Otherwise they will loose it.
+    // == 1; it wont loose any momentum
+    // > 1; it will gain momentum on each collision
+    restitution: 0.5
+  }
 )
 
 // We also need to add the behaviour to the world with addContactMaterial
@@ -80,7 +120,7 @@ const defaultContactMaterial = new CANNON.ContactMaterial(
 
 // We can also just change the property defaultContactMaterial of the World
 // This way, all Bodies from the World will have this material assigned to it
-world.defaultContactMaterial = defaultContactMaterial 
+world.defaultContactMaterial = defaultContactMaterial
 
 // In Cannon we create Body, which is like a mesh
 // We create a Shape first (just like in Three you would first create a geometry)
@@ -104,18 +144,18 @@ world.defaultContactMaterial = defaultContactMaterial
 // Add physics and collision to the floor
 const floorShape = new CANNON.Plane()
 const floorBody = new CANNON.Body( {
-// Mass 0 makes it not move no matter what happens in the World
-    mass: 0,
-    shape: floorShape,
-    // Not needed if we use world.defaultContactMaterial
-    //material: defaultMaterial
-})
+  // Mass 0 makes it not move no matter what happens in the World
+  mass: 0,
+  shape: floorShape,
+  // Not needed if we use world.defaultContactMaterial
+  //material: defaultMaterial
+} )
 
 floorBody.quaternion.setFromAxisAngle(
-    new CANNON.Vec3( -1, 0, 0 ),
-    Math.PI * 0.5
+  new CANNON.Vec3( -1, 0, 0 ),
+  Math.PI * 0.5
 )
-world.addBody(floorBody)
+world.addBody( floorBody )
 
 /**
  * Test sphere
@@ -137,122 +177,163 @@ world.addBody(floorBody)
  * Floor
  */
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshStandardMaterial({
-        color: '#777777',
-        metalness: 0.3,
-        roughness: 0.4,
-        envMap: environmentMapTexture,
-        envMapIntensity: 0.5
-    })
+  new THREE.PlaneGeometry( 10, 10 ),
+  new THREE.MeshStandardMaterial( {
+    color: '#777777',
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture,
+    envMapIntensity: 0.5
+  } )
 )
 floor.receiveShadow = true
 floor.rotation.x = - Math.PI * 0.5
-scene.add(floor)
+scene.add( floor )
 
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
-scene.add(ambientLight)
+const ambientLight = new THREE.AmbientLight( 0xffffff, 0.7 )
+scene.add( ambientLight )
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2)
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.2 )
 directionalLight.castShadow = true
-directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.mapSize.set( 1024, 1024 )
 directionalLight.shadow.camera.far = 15
 directionalLight.shadow.camera.left = - 7
 directionalLight.shadow.camera.top = 7
 directionalLight.shadow.camera.right = 7
 directionalLight.shadow.camera.bottom = - 7
-directionalLight.position.set(5, 5, 5)
-scene.add(directionalLight)
+directionalLight.position.set( 5, 5, 5 )
+scene.add( directionalLight )
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener( 'resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize( sizes.width, sizes.height )
+  renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2 ) )
+} )
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(- 3, 3, 3)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 100 )
+camera.position.set( - 3, 3, 3 )
+scene.add( camera )
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
+const controls = new OrbitControls( camera, canvas )
 controls.enableDamping = true
 
 /**
  * Renderer
  */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
+const renderer = new THREE.WebGLRenderer( {
+  canvas: canvas
+} )
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setSize( sizes.width, sizes.height )
+renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2 ) )
 
 // Array to apply positions to each one of the objects in the tick function
 const objectsToUpdate = []
 
-const sphereGeometry = new THREE.SphereGeometry(1, 20, 20 )
+const sphereGeometry = new THREE.SphereGeometry( 1, 20, 20 )
 const sphereMaterial = new THREE.MeshStandardMaterial( {
-    metalness: 0.3,
-    roughness: 0.4,
-    envMap: environmentMapTexture
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture
 } )
 //** Utils */
-const createSphere = (radius, position) =>
-{
-    // Create Three Mesh
-    const mesh = new THREE.Mesh( sphereGeometry, sphereMaterial )
-    mesh.scale.set(radius, radius, radius)
+const createSphere = ( radius, position ) => {
+  // Create Three Mesh
+  const mesh = new THREE.Mesh( sphereGeometry, sphereMaterial )
+  mesh.scale.set( radius, radius, radius )
 
-    mesh.castShadow = true
-    mesh.position.copy( position )
-    scene.add( mesh )
+  mesh.castShadow = true
+  mesh.position.copy( position )
+  scene.add( mesh )
 
-    // Create Cannon Body
-    const shape = new CANNON.Sphere()
-    const body = new CANNON.Body( {
-        mass: 1,
-        shape,
-        material: defaultMaterial
-    } )
+  // Create Cannon Body
+  const shape = new CANNON.Sphere( radius )
+  const body = new CANNON.Body( {
+    mass: 1,
+    shape,
+    position: new CANNON.Vec3( 0, 3, 0 ),
+    material: defaultMaterial
+  } )
 
-    body.position.copy( position )
-    world.addBody( body )
+  body.position.copy( position )
+  body.addEventListener( 'collide', ( e ) => {
+    playSound( e )
+  } )
+  world.addBody( body )
 
-    // Save in objectsToUpdate for later use. We
-    // push an object with the Mesh and the Body
-    objectsToUpdate.push( {mesh: mesh, body: body} )
+  // Save in objectsToUpdate for later use. We
+  // push an object with the Mesh and the Body
+  objectsToUpdate.push( { mesh: mesh, body: body } )
 
 }
 
-createSphere( 0.5, {x:0, y:3, z:0})
-createSphere( 0.5, {x:2, y:3, z:0})
-createSphere( 0.5, {x:4, y:3, z:0})
+createSphere( 0.5, { x: 0, y: 3, z: 0 } )
+createSphere( 0.5, { x: 2, y: 3, z: 0 } )
+createSphere( 0.5, { x: 4, y: 3, z: 0 } )
+// Create box
+
+const boxGeometry = new THREE.BoxGeometry( 1, 1, 1 )
+const boxMaterial = new THREE.MeshStandardMaterial( {
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture
+} )
+
+const createBox = ( width, height, depth, position ) => {
+  // Three.js mesh
+  const mesh = new THREE.Mesh( boxGeometry, boxMaterial )
+  mesh.scale.set( width, height, depth )
+  mesh.castShadow = true
+  mesh.position.copy( position )
+  scene.add( mesh )
+
+  // Cannon.js body
+  const shape = new CANNON.Box( new CANNON.Vec3( width * 0.5, height * 0.5, depth * 0.5 ) )
+
+  const body = new CANNON.Body( {
+    mass: 1,
+    position: new CANNON.Vec3( 0, 3, 0 ),
+    shape: shape,
+    material: defaultMaterial
+  } )
+  body.position.copy( position )
+  body.addEventListener( 'collide', ( e ) => {
+    playSound( e )
+  } )
+  world.addBody( body )
+
+  // Save in objects
+  objectsToUpdate.push( { mesh, body } )
+}
+
+//createBox( 1, 1, 1, { x: 3, y: 3, z: 0 } )
+//createBox( 0.5, { x: 3, y: 3, z: 2 } )
+//createBox( 0.5, { x: 2, y: 3, z: 7 } )
 
 /**
  * Animate
@@ -260,38 +341,37 @@ createSphere( 0.5, {x:4, y:3, z:0})
 const clock = new THREE.Clock()
 let oldElapsedTime = 0
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - oldElapsedTime
-    oldElapsedTime = elapsedTime
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - oldElapsedTime
+  oldElapsedTime = elapsedTime
 
-    // Update physics world
+  // Update physics world
 
-    // Apply wind force on each frame. When the momentum from the localForce ends,
-    // this one will cause the ball to move the opposite side
-    //sphereBody.applyForce( new CANNON.Vec3(- 0.5, 0, 0), sphereBody.position )
+  // Apply wind force on each frame. When the momentum from the localForce ends,
+  // this one will cause the ball to move the opposite side
+  //sphereBody.applyForce( new CANNON.Vec3(- 0.5, 0, 0), sphereBody.position )
 
-    //1 If we want the app to run at 60 frames, we should indicate 1/60
-    //2 Time passed since the last frame (deltaTime)
-    //3 Number of frames that you give to the physics world in case of delay
-    world.step( 1 / 60, deltaTime, 3 )
-    
-    for ( const object of objectsToUpdate )
-    {
-        object.mesh.position.copy( object.body.position )
-    }
+  //1 If we want the app to run at 60 frames, we should indicate 1/60
+  //2 Time passed since the last frame (deltaTime)
+  //3 Number of frames that you give to the physics world in case of delay
+  world.step( 1 / 60, deltaTime, 3 )
 
-    // Apply physics world update to the scene
-    //sphere.position.copy( sphereBody.position )
-    // Update controls
-    controls.update()
+  for ( const object of objectsToUpdate ) {
+    object.mesh.position.copy( object.body.position )
+    object.mesh.quaternion.copy( object.body.quaternion )
+  }
 
-    // Render
-    renderer.render(scene, camera)
+  // Apply physics world update to the scene
+  //sphere.position.copy( sphereBody.position )
+  // Update controls
+  controls.update()
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Render
+  renderer.render( scene, camera )
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame( tick )
 }
 
 tick()
